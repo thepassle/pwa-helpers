825d58901dfa6691f61b0dd03afdf6b357a856e7
Passle <pascalschilp@gmail.com>
2019-10-04 22:39:23 +0200

    test: test prompt is called

--- test/pwa-install-button.test.js
+++ test/pwa-install-button.test.js
@@ -56,6 +56,7 @@ describe('PwaInstallButton', () => {
 
     el.click();
     
+    expect(el._deferredPrompt.prompt).called;
     expect(el.hidden).to.equal(true);
     await nextFrame();
     expect(el._deferredPrompt).to.equal(null);
@@ -77,7 +78,7 @@ describe('PwaInstallButton', () => {
     }
 
     el.click();
-
+    expect(el._deferredPrompt.prompt).called;
     expect(el.hidden).to.equal(false);
   });
 });
