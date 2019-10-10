c34f38abe006170f9e8a82897400653a8159b533
Passle <pascalschilp@gmail.com>
2019-10-10 21:17:06 +0200

    test: add tests for pwa-update-available

--- test/pwa-update-available.test.js
+++ test/pwa-update-available.test.js
@@ -4,18 +4,48 @@ import sinon from 'sinon'; // eslint-disable-line
 import '../pwa-update-available.js';
 
 describe('PwaUpdateAvailable', () => {
-  it('is hidasdasdden by default', async () => {
+  it('is hidden by default', async () => {
     const el = await fixture(html`
       <pwa-update-available></pwa-update-available>
     `);
 
-    expect(el).to.equal(el);
+    expect(el.hidden).to.equal(true);
+  });
+
+  it('posts a message', async () => {
+    const el = await fixture(html`
+      <pwa-update-available></pwa-update-available>
+    `);
+    el.hidden = false;
+    el._newWorker = {
+      postMessage: sinon.spy(),
+    };
+    el.click();
+    expect(el._newWorker.postMessage).calledOnceWith('skipWaiting');
+  });
+
+  it('is visible when updatefound and statechange has been fired', async () => {
+    const addEventListener = (_, cb) => cb();
+
+    Object.defineProperty(window.navigator, 'serviceWorker', {
+      writable: true,
+      value: {
+        controller: true,
+        getRegistration: async () => ({
+          installing: {
+            state: 'installed',
+            addEventListener,
+          },
+          addEventListener,
+        }),
+        addEventListener: () => {},
+      },
+    });
+
+    const el = await fixture(html`
+      <pwa-update-available></pwa-update-available>
+    `);
+
+    expect(el.hidden).to.equal(false);
   });
 });
-
-/**
- * - is hidden by default
- * - shows slotted content
- * - posts message and becomes hidden
- * - should reload on controllerchange
- */
