c20ebebed206ad4dcdc30a4e4e0347229ba4bb5d
Passle <pascalschilp@gmail.com>
2019-10-21 22:22:04 +0200

    test: improve coverage

--- test/pwa-update-available.test.js
+++ test/pwa-update-available.test.js
@@ -1,4 +1,4 @@
-import { html, fixture, expect } from '@open-wc/testing';
+import { html, fixture, expect, oneEvent } from '@open-wc/testing';
 import sinon from 'sinon';
 
 import '../pwa-update-available.js';
@@ -24,7 +24,7 @@ describe('PwaUpdateAvailable', () => {
     expect(el._newWorker.postMessage).calledOnceWith({ type: 'SKIP_WAITING' });
   });
 
-  it('is visible when updatefound and statechange has been fired', async () => {
+  it('is visible and fires a `pwa-update-available` event when updatefound and statechange has been fired', async () => {
     const addEventListener = (_, cb) => cb();
 
     Object.defineProperty(window.navigator, 'serviceWorker', {
@@ -42,14 +42,17 @@ describe('PwaUpdateAvailable', () => {
       },
     });
 
-    const el = await fixture(html`
-      <pwa-update-available></pwa-update-available>
-    `);
+    const el = document.createElement('pwa-update-available');
+    setTimeout(async () => {
+      await el.connectedCallback();
+    });
+    const { detail } = await oneEvent(el, 'pwa-update-available');
 
+    expect(detail).to.equal(true);
     expect(el.hidden).to.equal(false);
   });
 
-  it('is visible when there already is a waiting service worker', async () => {
+  it('is visible and fires a `pwa-update-available` event when there already is a waiting service worker', async () => {
     const addEventListener = (_, cb) => cb();
 
     Object.defineProperty(window.navigator, 'serviceWorker', {
@@ -68,10 +71,43 @@ describe('PwaUpdateAvailable', () => {
       },
     });
 
-    const el = await fixture(html`
-      <pwa-update-available></pwa-update-available>
-    `);
+    const el = document.createElement('pwa-update-available');
+    setTimeout(async () => {
+      await el.connectedCallback();
+    });
+    const { detail } = await oneEvent(el, 'pwa-update-available');
 
+    expect(detail).to.equal(true);
+    expect(el.hidden).to.equal(false);
+  });
+
+  it('refreshes the page after controllerchange', async () => {
+    const addEventListener = (_, cb) => cb();
+
+    Object.defineProperty(window.navigator, 'serviceWorker', {
+      writable: true,
+      value: {
+        controller: true,
+        getRegistration: async () => ({
+          installing: {
+            state: 'not installed',
+            addEventListener,
+          },
+          waiting: true,
+          addEventListener,
+        }),
+        addEventListener,
+      },
+    });
+
+    const el = document.createElement('pwa-update-available');
+    el._refreshing = true;
+    setTimeout(async () => {
+      await el.connectedCallback();
+    });
+    const { detail } = await oneEvent(el, 'pwa-update-available');
+
+    expect(detail).to.equal(true);
     expect(el.hidden).to.equal(false);
   });
 });
